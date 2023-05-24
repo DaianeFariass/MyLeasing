7efa252e02d0efea38a7f4c91369cab11aa5f8ea
Daiane Farias <daiane.farias.contractor@gmail.com>
2023-05-24 16:12:59 +0100

    CRUD Owners

---
+++ MyLeasing.Commom/Data/Entities/Owner.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyLeasing.Web.Data.Entities
+{
+    public class Owner
+    {
+        public int Id { get; set; } 
+        public int Document { get; set; }
+
+        [Display(Name = "Owner Name")]
+        public string OwnerName { get; set; }
+
+        [Display(Name = "Fixed Phone")]
+        public int FixedPhone { get; set; }
+
+        [Display(Name = "Cell Phone")]
+        public int CellPhone { get; set; }
+
+        public string Address { get; set; }
+
+        
+    }
+}
---
+++ MyLeasing.Commom/Migrations/20230524134049_InitDb.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyLeasing.Commom.Migrations
+{
+    public partial class InitDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Owners",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Document = table.Column<int>(type: "int", nullable: false),
+                    OwnerName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FixedPhone = table.Column<int>(type: "int", nullable: false),
+                    CellPhone = table.Column<int>(type: "int", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Owners", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Owners");
+        }
+    }
+}
